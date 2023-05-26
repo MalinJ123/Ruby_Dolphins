84249c5afd2fd7916bdbaf837a4df15e9847e9ae
NorthPew <vbergstrom02@gmail.com>
2023-05-26 18:22:45 +0200

    Added react ground to project

---
+++ src/routeConfig.jsx
@@ -0,0 +1,12 @@
+import { createHashRouter } from "react-router-dom";
+
+//import Root from "./routes/Root";
+
+const router = createHashRouter([
+    {
+        path: '/',
+        //element: <Root />,
+    }
+])
+
+export { router }
