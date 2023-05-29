19294121d8f2d6b7d20620e70736260b3b84e524
NorthPew <vbergstrom02@gmail.com>
2023-05-29 16:20:26 +0200

    Used the test script and fixed all issues it found

--- api/routes/users.js
+++ api/routes/users.js
@@ -35,10 +35,10 @@ router.get("/:id", async (req, res) => {
 
         if (!user) {
             return res.status(404).send("User not found.");
+        } else {
+            console.log("Visar användaren med ID:", user);
+            res.send(user);
         }
-
-        console.log("Visar användaren med ID:", user);
-        res.send(user);
     } catch (error) {
         console.log("Ett fel uppstod vid hämtning av användare:", error);
         res.status(500).send("Ett fel uppstod vid hämtning av användare.");
@@ -50,14 +50,14 @@ router.post("/", async (req, res) => {
     await db.read();
     console.log("test 1");
 
-    let username = req.body.username;
+    let name = req.body.name;
     let password = req.body.password;
     function generateId() {
         return Math.floor(Math.random() * 10000);
     }
     let newUser = {
         id: generateId(),
-        username,
+        name,
         password,
     };
     console.log("newUser", newUser);
@@ -80,7 +80,7 @@ router.delete("/all", async (req, res) => {
 	await db.read();
 	db.data.users = [];
 	await db.write();
-	res.sendStatus(204);
+	res.sendStatus(200);
 })
 
 router.delete("/:id", async (req, res) => {
@@ -97,7 +97,7 @@ router.delete("/:id", async (req, res) => {
         db.data.users = db.data.users.filter((user) => user.id !== id);
         await db.write();
         console.log("test 3");
-        return res.sendStatus(204);
+        return res.sendStatus(200);
     }
 });
 
@@ -119,7 +119,7 @@ router.delete("/:id", async (req, res) => {
             return res.status(400).send("Kunde inte hitta användaren, kontrollera att Id  är korrekt");
         } 
             
-        oldUser.username = editedUser.username;
+        oldUser.name = editedUser.name;
         oldUser.password = editedUser.password;
 
         db.data.users[oldUser] = editedUser;
