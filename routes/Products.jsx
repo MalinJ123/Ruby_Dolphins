b8c2af5eb572eeadb0497b75498b0c977ef432a9
MalinJ123 <malin--jonsson@hotmail.com>
2023-05-31 00:07:48 +0200

    bättre cards och sökfunktion

--- routes/Products.jsx
+++ routes/Products.jsx
@@ -1,46 +1,65 @@
 import { useLoaderData, Link } from "react-router-dom";
-import  "../stylesheet/products.css"
+import "../stylesheet/products.css";
 import { getProducts } from "../data/getProducts";
+import { useState, useEffect } from "react";
 
 export const loader = () => getProducts();
 
 function Products() {
 	const productData = useLoaderData();
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const handleChange = () => {
+		setSearchTerm(event.target.value);
+	};
 
 	return (
 		<div>
-			<h1>Produkter</h1>
+            <h3 className="produkt">Produkter</h3>
+			<section className="top-searchbar">
+				<div className="search-bar">
+					<input
+						onChange={handleChange}
+						value={searchTerm}
+						className="search-input"
+						type="text"
+						placeholder="Sök efter produkter"
+					/>
+				</div>
+			</section>
+				
+			<div className="grid-container">
+				{productData 
+					.filter((product) =>
+						product.name
+							.toLowerCase()
+							.includes(searchTerm.toLowerCase())
+					)
+					.map((product) => (
+						<div className="product" key={product.id}>
+							<h3 className="">
+								<Link
+									className="products-title"
+									to={`/products/${product.id}`}
+								>
+									{product.name}
+								</Link>
+							</h3>
+							<img
+								className="products-pic"
+								src={product.image}
+								alt="product image"
+							/>
+							<div>
+								<p className="product-price">
+									Pris: {product.price} kr
+								</p>
+								<p>Taggar: {product.tags.join(", ")}</p>
 
-            <div className="grid-container">
-				{productData.map((product) => (
-					<div className="product" key={product.id}>
-						<h3 className="">
-							<Link
-								className="products-title"
-								to={`/products/${product.id}`}
-							>
-								{product.name}
-							</Link>
-						</h3>
-						<img
-							className="products-pic"
-							src={product.image}
-							alt="product image"
-						/>
-						<div className="product-price">
-							<p>Pris: {product.price}  kr</p>
-							<p>Taggar: {product.tags} </p>
-
+							</div>
 						</div>
-						
-					</div>
-				))}
+					))}
 			</div>
-
-
-
-
-
 		</div>
 	);
 }
