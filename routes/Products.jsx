d2bcd05c6539702f987e1dbf2f4dda706782779f
MalinJ123 <malin--jonsson@hotmail.com>
2023-05-30 15:20:22 +0200

    produkt sida på g

--- routes/Products.jsx
+++ routes/Products.jsx
@@ -1,25 +1,48 @@
 import { useLoaderData, Link } from "react-router-dom";
-
-import {getProducts} from "../data/getProducts";
+import  "../stylesheet/products.css"
+import { getProducts } from "../data/getProducts";
 
 export const loader = () => getProducts();
 
 function Products() {
-    const productData = useLoaderData()
-    return (
-        <div>
-            <h1>Products</h1>
-            <ul>
-                {
-                    productData.map((product) => (
-                        <li key={product.id}>
-                            <Link to={`/products/${product.id}`}>{product.name}</Link>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    )
+	const productData = useLoaderData();
+
+	return (
+		<div>
+			<h1>Produkter</h1>
+
+            <div className="grid-container">
+				{productData.map((product) => (
+					<div className="product" key={product.id}>
+						<h3 className="">
+							<Link
+								className="products-title"
+								to={`/products/${product.id}`}
+							>
+								{product.name}
+							</Link>
+						</h3>
+						<img
+							className="products-pic"
+							src={product.image}
+							alt="product image"
+						/>
+						<div className="product-price">
+							<p>Pris: {product.price}  kr</p>
+							<p>Taggar: {product.tags} </p>
+
+						</div>
+						
+					</div>
+				))}
+			</div>
+
+
+
+
+
+		</div>
+	);
 }
 
-export default Products
+export default Products;
