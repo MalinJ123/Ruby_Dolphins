2ad63bd718eefc9f7ed0224e44385ffe1434c05a
Mustafa <mustafazeinali@gmail.com>
2023-05-31 16:19:44 +0200

    det löst en del

--- components/UpdateProduct.jsx
+++ components/UpdateProduct.jsx
@@ -1,28 +1,42 @@
 import { useState } from "react";
 import "./UpdateProduct.css";
 import { updateProduct } from "../data/updateProduct.js";
-import { getProducts } from "../data/getProducts.js";
+//import { getProducts } from "../data/getProducts.js";
 
 const ToUpdateProduct = () => {
     const [product , setProduct] = useState({
+        id:"" ,
         name: "",
         price: "",
         image: "",
         tags: "",
     });
 
-   getProducts.push(ToUpdateProduct)
+   
 
     
 
-    function handleProduct (){
+    async function handleProduct  (e){
+        e.preventDefault();
         const changeItem ={
+            id: product.id,
             name: product.name,
             price: product.price,
             image: product.image,
             tags: product.tags
         }
-        updateProduct(changeItem)
+        console.log("skickar detta till api", changeItem);
+        try{
+           const result = await updateProduct(changeItem)
+           if (result){
+            console.log("uppdateringen lyckades");
+           }else{
+            console.log("något blev fel");
+           }
+
+        } catch (error){
+            console.log(error.message);
+        }
     }
 
 
@@ -31,32 +45,52 @@ const ToUpdateProduct = () => {
 
     return (
         <section>
-            <form action="" className="form"> 
-                <section className="input-item input-name"> 
-                    <label htmlFor=""></label>
-                    <input type="text" placeholder="Name" value={product.name}
+            <form onSubmit={handleProduct} className="form"> 
+            <div className="update-form-div">
+
+                
+                <div className="update-input-div">
+                    <label htmlFor="product-Id">Id: </label>
+                    <input id="product-Id" type="number" placeholder="Id" 
+                    onChange={(e) => setProduct({...product, id: e.target.value})}/> 
+
+                </div>
+                <div className="update-input-div">
+                    <label htmlFor="product-name">Produktnamn: </label>
+                    <input id="product-name" type="text" placeholder="Name" 
                     onChange={(e) => setProduct({...product, name: e.target.value})}/> 
-                </section>
 
-                <section className="input-item input-price"> 
-                    <label htmlFor=""></label>
-                    <input type="number" placeholder="Price"  value={product.price}
+                </div>
+              
+
+                
+                <div className="update-input-div">
+                    
+                    <label htmlFor="price">Produktpris: </label>
+                    <input id="price" type="number" placeholder="Price"  
                     onChange={(e) => setProduct ({...product, price : e.target.value})}/> 
-                </section>
-
-                <section className="input-item input-tags"> 
-                    <label htmlFor=""></label>
-                    <input type="text" placeholder="tags"  value={product.tags}
-                    onChange={(e) => setProduct ({...product, tags : e.target.value})}/> 
-                </section>
-
-                <section className="add-file"> 
-                    <label htmlFor=""></label>
-                    <input type="file" placeholder="file" value={product.image}
-                    onChange={(e) => setProduct({...product , image : e.target.value})}/> 
-                </section>
+                </div >
             
-                <button onClick={handleProduct}>Click</button>
+
+             
+                <div className="update-input-div"> 
+                    <label htmlFor="tags">Produkttaggar: </label>
+                    <input id="tags" type="text" placeholder="bad, kräm" 
+                    onChange={(e) => setProduct ({...product, tags : e.target.value})}/> 
+                </div>
+              
+               
+                
+                <div className="update-input-div">
+                    <label htmlFor="image">Produktbild: (url)</label>
+                    <input id="image" type="text" placeholder="https://elgiganten.se ..." 
+                    onChange={(e) => setProduct({...product , image : e.target.value})}/> 
+                    
+                </div>
+               
+                
+                <button type="submit">Click</button>
+                    </div>
              </form>
         </section>
      
--- data/updateProduct.js
+++ data/updateProduct.js
@@ -1,14 +1,25 @@
 import { API_URL } from "./constants.js"
 
 async function updateProduct(product) {
+    
+    const data = {
+        id: Number(product.id ) ,
+        name: product.name,
+        price: Number(product.price ) ,
+        image: product.image,
+        tags: [
+            product.tags
+        ]
+    }
     const options ={
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(product)
+        body: JSON.stringify(data)
+        
     }
-    const response = await fetch( API_URL + 'products/', options)
+    const response = await fetch( API_URL + 'products/:id', options)
     const statusObject = await response.json()
     console.log("response from Api",statusObject);
 }
