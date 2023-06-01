1622507bd2d4e557bbd9ab4972d1870c607c0f75
Mustafa <mustafazeinali@gmail.com>
2023-06-01 10:53:02 +0200

    put product är klar

--- data/updateProduct.js
+++ data/updateProduct.js
@@ -1,27 +1,24 @@
-import { API_URL } from "./constants.js"
+import { API_URL } from "./constants.js";
 
 async function updateProduct(product) {
-    
-    const data = {
-        id: Number(product.id ) ,
-        name: product.name,
-        price: Number(product.price ) ,
-        image: product.image,
-        tags: [
-            product.tags
-        ]
-    }
-    const options ={
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-        
-    }
-    const response = await fetch( API_URL + 'products/:id', options)
-    const statusObject = await response.json()
-    console.log("response from Api",statusObject);
+  //let tag = product.tags[tags.length]
+  const data = {
+    id: Number(product.id),
+    name: product.name,
+    price: Number(product.price),
+    image: product.image,
+    tags: [product.tags],
+  };
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(API_URL + "products/" + product.id, options);
+  const statusObject = await response.json();
+  console.log("response from Api", statusObject);
 }
 
-export { updateProduct }
+export { updateProduct };
