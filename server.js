eadeec5c619ddea86047022b1d9ba798abcbb7e5
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-06-02 13:08:23 +0200

    update port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import usersRouter from "./api/routes/users.js";
 import searchQuery from "./api/routes/search.js";
 
 // Express saker
-const PORT = 666
+const PORT = 7894
 const app = express()
 
 // The middlemen are looking at us
