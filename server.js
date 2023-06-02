0c773d7e4159e0a21df80bdd8b6048e85e354659
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-06-02 13:10:58 +0200

    dist

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
 // Routes //
 const whereWeAre = dirname(fileURLToPath(import.meta.url))
-const dist = join(whereWeAre, '../dist')
+const dist = join(whereWeAre, './dist')
 app.use( express.static(dist) )
 // -> Start
 
